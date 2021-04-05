6206f84af2b30c281a027ae19cec849b5e07e68a
= <tomclifford@btinternet.com>
2021-04-05 12:49:23 +0100

    Initial Commit

---
+++ DS3/DS3App.swift
@@ -0,0 +1,17 @@
+//
+//  DS3App.swift
+//  DS3
+//
+//  Created by Tom Clifford on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DS3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
